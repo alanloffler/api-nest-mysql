ab4ee29e6b790ef93ec7ddc07e11efcf3afd5b43
alanloffler <alanmatiasloffler@gmail.com>
2024-03-30 11:50:25 -0300

    Entity options changed for user table

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,22 +1,24 @@
-import { Property } from 'src/properties/entities/property.entity';
-import { Column, CreateDateColumn, Entity, OneToMany, UpdateDateColumn } from 'typeorm';
+import { Column, CreateDateColumn, DeleteDateColumn, Entity, OneToMany, UpdateDateColumn } from 'typeorm';
+
+import { Role } from '../../common/enums/role.enum';
+import { Property } from '../../properties/entities/property.entity';
 
 @Entity()
 export class User {
     @Column({ primary: true, generated: true })
     id: number;
 
-    @Column()
+    @Column({ nullable: true })
     name: string;
 
     @Column()
     email: string;
 
-    @Column()
+    @Column({ unique: true, nullable: false, select: false })
     password: string;
 
-    @Column({ default: 'user' })
-    type: string;
+    @Column({ type: 'enum', enum: Role, default: Role.USER })
+    role: Role;
 
     @Column({ nullable: true })
     phone: string;
@@ -26,6 +28,9 @@ export class User {
 
     @UpdateDateColumn()
     updated_at: Date;
+
+    @DeleteDateColumn()
+    deleted_at: Date;
 
     @OneToMany(() => Property, (property) => property.created_by)
     properties: Property[];
