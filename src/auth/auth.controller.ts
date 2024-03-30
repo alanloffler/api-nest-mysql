689562510d052d6ab1deb65dd7cb85fa8af0233c
alanloffler <alanmatiasloffler@gmail.com>
2024-03-30 11:55:15 -0300

    Deleted test profile route

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,12 +1,7 @@
-import { Body, Controller, Get, Post, Request, UseGuards } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { RegisterDto } from './dto/register.dto';
 import { LoginDto } from './dto/login.dto';
-import { IActiveUser } from '../common/interfaces/active-user.interface';
-import { AuthGuard } from './guards/auth.guard';
-import { Roles } from './decorators/roles.decorator';
-import { Role } from '../common/enums/role.enum';
-import { RolesGuard } from './guards/roles.guard';
 
 @Controller('auth')
 export class AuthController {
@@ -20,12 +15,5 @@ export class AuthController {
     @Post('login')
     login(@Body() loginDto: LoginDto) {
         return this.authService.login(loginDto);
-    }
-
-    @Get('profile')
-    @Roles(Role.USER)
-    @UseGuards(AuthGuard, RolesGuard)
-    async profile(@Request() user: IActiveUser) {
-        return await this.authService.profile(user);
     }
 }
