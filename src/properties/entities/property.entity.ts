5b8f9cb9d7134b05070863c18f522bf0e1896eef
alanloffler <alanmatiasloffler@gmail.com>
2024-03-30 19:23:30 -0300

    New relation on column created_by (User)

--- src/properties/entities/property.entity.ts
+++ src/properties/entities/property.entity.ts
@@ -1,32 +1,48 @@
-import { User } from 'src/users/entities/user.entity';
-import { Column, CreateDateColumn, Entity, ManyToOne, UpdateDateColumn } from 'typeorm';
+import { Column, CreateDateColumn, DeleteDateColumn, Entity, JoinColumn, ManyToOne, UpdateDateColumn } from 'typeorm';
+import { User } from '../../users/entities/user.entity';
 
 @Entity()
 export class Property {
     @Column({ primary: true, generated: true })
     id: number;
+
     @Column()
     type: string;
+
     @Column()
     color: string;
+
     @Column()
     business_type: string;
+
     @Column({ type: 'tinyint' })
     is_active: number;
+
     @Column()
     title: string;
+
     @Column()
     short_description: string;
+
     @Column({ type: 'longtext' })
     long_description: string;
+
     @Column()
     price: number;
 
     @CreateDateColumn()
     created_at: Date;
+
     @UpdateDateColumn()
     updated_at: Date;
 
-    @ManyToOne(() => User, (user) => user.id, { eager: true })
-    created_by: User;
+    @DeleteDateColumn()
+    deleted_at: Date;
+
+    @ManyToOne(() => User)
+    @JoinColumn({ name: 'created_by', referencedColumnName: 'id' })
+    user: User;
+
+    @Column()
+    created_by: number;
 }
