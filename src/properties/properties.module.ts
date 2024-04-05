d29ae68f691dcfb493dd13674814c4e3cafa14a3
alanloffler <alanmatiasloffler@gmail.com>
2024-04-05 15:30:29 -0300

    Added image storage on disk

--- src/properties/properties.module.ts
+++ src/properties/properties.module.ts
@@ -1,29 +1,15 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { MulterModule } from '@nestjs/platform-express';
 import { Property } from './entities/property.entity';
 import { UsersModule } from '../users/users.module';
 import { PropertiesController } from './properties.controller';
 import { PropertiesService } from './properties.service';
 import { UsersService } from '../users/users.service';
-import { diskStorage } from 'multer';
 
 @Module({
-    imports: [
-        TypeOrmModule.forFeature([Property]),
-        MulterModule.registerAsync({
-            useFactory: () => ({
-                storage: diskStorage({
-                    destination: './uploads',
-                    filename: (req: Express.Request, file: Express.Multer.File, callback: any) => {
-                        callback(null, crypto.randomUUID() + '.' + file.originalname.split('.').pop());
-                    },
-                }),
-            }),
-        }),
-        UsersModule,
-    ],
+    imports: [TypeOrmModule.forFeature([Property]), UsersModule],
     controllers: [PropertiesController],
     providers: [PropertiesService, UsersService],
+    exports: [TypeOrmModule, PropertiesService],
 })
 export class PropertiesModule {}
