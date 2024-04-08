463746bcc6b4617b7fc46081015ea2237b880fda
alanloffler <alanmatiasloffler@gmail.com>
2024-04-08 14:59:31 -0300

    subscriber test

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
-import { UsersModule } from './users/users.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { PropertiesModule } from './properties/properties.module';
 import { AuthModule } from './auth/auth.module';
+import { CategoriesModule } from './categories/categories.module';
 import { ImagesModule } from './images/images.module';
+import { PropertiesModule } from './properties/properties.module';
+import { UsersModule } from './users/users.module';
+import { CategorySubscriber } from './categories/category.subscriber';
 
 @Module({
     imports: [
@@ -16,11 +18,13 @@ import { ImagesModule } from './images/images.module';
             database: 'api-nest',
             autoLoadEntities: true,
             synchronize: true,
+            subscribers: [CategorySubscriber],
         }),
-        UsersModule,
-        PropertiesModule,
         AuthModule,
+        CategoriesModule,
         ImagesModule,
+        PropertiesModule,
+        UsersModule,
     ],
     controllers: [],
     providers: [],
