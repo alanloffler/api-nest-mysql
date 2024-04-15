7da4533abc491124cc2f2e2c2f1aa7c02e6e08d3
alanloffler <alanmatiasloffler@gmail.com>
2024-04-15 08:58:18 -0300

    Order

--- src/auth/decorators/auth.decorator.ts
+++ src/auth/decorators/auth.decorator.ts
@@ -1,8 +1,7 @@
 import { UseGuards, applyDecorators } from '@nestjs/common';
-
+import { AuthGuard } from '../guards/auth.guard';
 import { Role } from '../../common/enums/role.enum';
 import { Roles } from './roles.decorator';
-import { AuthGuard } from '../guards/auth.guard';
 import { RolesGuard } from '../guards/roles.guard';
 
 export function Auth(role: Role) {
--- src/auth/decorators/roles.decorator.ts
+++ src/auth/decorators/roles.decorator.ts
@@ -1,5 +1,4 @@
 import { SetMetadata } from '@nestjs/common';
-
 import { Role } from '../../common/enums/role.enum';
 
 export const ROLES_KEY = 'roles';
