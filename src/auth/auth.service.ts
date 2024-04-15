5a0566d469569c81fdbb3a51e3cdea4ce30aa0ab
alanloffler <alanmatiasloffler@gmail.com>
2024-04-15 08:58:08 -0300

    Error handling

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,9 +1,9 @@
-import { UnauthorizedException, Injectable, BadRequestException } from '@nestjs/common';
-import { JwtService } from '@nestjs/jwt';
 import * as bcryptjs from 'bcryptjs';
-import { UsersService } from '../users/users.service';
-import { RegisterDto } from '../auth/dto/register.dto';
+import { JwtService } from '@nestjs/jwt';
+import { Injectable, HttpException, HttpStatus } from '@nestjs/common';
 import { LoginDto } from './dto/login.dto';
+import { RegisterDto } from '../auth/dto/register.dto';
+import { UsersService } from '../users/users.service';
 
 @Injectable()
 export class AuthService {
@@ -14,8 +14,7 @@ export class AuthService {
 
     async register(registerDto: RegisterDto) {
         const user = await this.usersService.findOneByEmail(registerDto.email);
-
-        if (user) throw new BadRequestException('User already exists');
+        if (user) throw new HttpException('User already exists (email)', HttpStatus.BAD_REQUEST);
 
         const passwordHashed = await bcryptjs.hash(registerDto.password, 10);
 
@@ -24,10 +23,10 @@ export class AuthService {
 
     async login(loginDto: LoginDto) {
         const user = await this.usersService.findByEmailWithPassword(loginDto.email);
-        if (!user) throw new UnauthorizedException('E-mail not found');
+        if (!user) throw new HttpException('E-mail not found', HttpStatus.UNAUTHORIZED);
 
         const passwordMatch = await bcryptjs.compare(loginDto.password, user.password);
-        if (!passwordMatch) throw new UnauthorizedException('Password incorrect');
+        if (!passwordMatch) throw new HttpException('Password incorrect', HttpStatus.UNAUTHORIZED);
 
         const payload = { id: user.id, email: user.email, role: user.role };
         const token = await this.jwtService.signAsync(payload);
