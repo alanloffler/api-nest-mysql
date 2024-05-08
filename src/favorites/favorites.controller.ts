e6574eb68d1aefee4a27d7f364ea5a05383f516d
alanloffler <alanmatiasloffler@gmail.com>
2024-05-08 18:10:07 -0300

    Favorites implemented

--- src/favorites/favorites.controller.ts
+++ src/favorites/favorites.controller.ts
@@ -1,6 +1,6 @@
+import { Controller, Get, Post, Param, Delete, ParseIntPipe } from '@nestjs/common';
 import { ActiveUser } from '../common/decorators/active-user.decorator';
 import { Auth } from '../auth/decorators/auth.decorator';
-import { Controller, Get, Post, Param, Delete, ParseIntPipe } from '@nestjs/common';
 import { FavoritesService } from './favorites.service';
 import { IActiveUser } from '../common/interfaces/active-user.interface';
 import { Role } from '../common/enums/role.enum';
@@ -8,25 +8,20 @@ import { Role } from '../common/enums/role.enum';
 @Auth(Role.USER)
 @Controller('favorites')
 export class FavoritesController {
-  constructor(private readonly favoritesService: FavoritesService) {}
+    constructor(private readonly favoritesService: FavoritesService) {}
 
-  @Post(':id')
-  create(@Param('id', ParseIntPipe) id: number, @ActiveUser() activeUser: IActiveUser) {
-    return this.favoritesService.create(id, activeUser);
-  }
+    @Post(':id')
+    create(@Param('id', ParseIntPipe) id: number, @ActiveUser() activeUser: IActiveUser) {
+        return this.favoritesService.create(id, activeUser);
+    }
 
-  @Get()
-  findAll(@ActiveUser() activeUser: IActiveUser) {
-    return this.favoritesService.findAll(activeUser);
-  }
+    @Get()
+    findAll(@ActiveUser() activeUser: IActiveUser) {
+        return this.favoritesService.findAll(activeUser);
+    }
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.favoritesService.findOne(+id);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.favoritesService.remove(+id);
-  }
+    @Delete(':id')
+    remove(@Param('id', ParseIntPipe) id: number) {
+        return this.favoritesService.remove(id);
+    }
 }
--- src/favorites/favorites.service.ts
+++ src/favorites/favorites.service.ts
@@ -1,55 +1,51 @@
 import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
-import { IActiveUser } from '../common/interfaces/active-user.interface';
-import { UsersService } from '../users/users.service';
 import { InjectRepository } from '@nestjs/typeorm';
-import { Favorite } from './entities/favorite.entity';
 import { Repository } from 'typeorm';
+import { Favorite } from './entities/favorite.entity';
+import { IActiveUser } from '../common/interfaces/active-user.interface';
 import { PropertiesService } from '../properties/properties.service';
-import { Role } from 'src/common/enums/role.enum';
+import { Role } from '../common/enums/role.enum';
+import { UsersService } from '../users/users.service';
 
 @Injectable()
 export class FavoritesService {
     constructor(
         @InjectRepository(Favorite) private favoriteRepository: Repository<Favorite>,
         private readonly propertysService: PropertiesService,
-        private readonly usersService: UsersService
+        private readonly usersService: UsersService,
     ) {}
 
     async create(propertyId: number, activeUser: IActiveUser) {
+        console.log(propertyId, activeUser);
         const user = await this.usersService.findOne(activeUser.id);
         if (!user) throw new HttpException('User not found', HttpStatus.NOT_FOUND);
-
         const property = await this.propertysService.validateProperty(propertyId);
         this.propertysService.validateSameUser(property, activeUser);
-
-        const createFavorite = this.favoriteRepository.create({ propertyId, userId: activeUser.id });
+        const createFavorite = this.favoriteRepository.create({ propertyId: property.id, userId: activeUser.id });
         const createdFavorite = await this.favoriteRepository.save(createFavorite);
+        console.log(createdFavorite);
         if (!createdFavorite) throw new HttpException('Favorite not created', HttpStatus.BAD_REQUEST);
-        
         return { message: 'Favorite created', statusCode: HttpStatus.OK };
     }
 
     async findAll(activeUser: IActiveUser) {
         if (activeUser.role === Role.ADMIN)
             return await this.favoriteRepository.find({
+                where: { userId: activeUser.id },
                 withDeleted: true,
-                order: { id: 'DESC' }
+                order: { id: 'DESC' },
             });
-        
         const favorites = await this.favoriteRepository.find({
             where: { userId: activeUser.id },
-            order: { id: 'DESC' }
+            order: { id: 'DESC' },
         });
         if (!favorites) throw new HttpException('Favorites not found', HttpStatus.NOT_FOUND);
-
         return favorites;
     }
 
-    findOne(id: number) {
-        return `This action returns a #${id} favorite`;
-    }
-
-    remove(id: number) {
-        return `This action removes a #${id} favorite`;
+    async remove(id: number) {
+        const deleteFavorite = await this.favoriteRepository.delete({ propertyId: id });
+        if (deleteFavorite.affected === 0) throw new HttpException('Favorite not deleted', HttpStatus.BAD_REQUEST);
+        return { message: 'Favorite deleted', statusCode: HttpStatus.OK };
     }
 }
