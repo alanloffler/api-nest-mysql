0012b25ff6865fda92917821ee950c957ae3af6b
alanloffler <alanmatiasloffler@gmail.com>
2024-05-23 20:47:29 -0300

    Dto changed by entity changes

--- src/cities/dto/create-city.dto.ts
+++ src/cities/dto/create-city.dto.ts
@@ -2,17 +2,10 @@ import { IsString, MinLength } from 'class-validator';
 import { Transform } from 'class-transformer';
 
 export class CreateCityDto {
-    @Transform(({ value }) => value.trim())
-    @Transform(({ value }) => value.toLowerCase())
+    @Transform(({ value }) => value.trim().toLowerCase())
     @IsString()
     @MinLength(1)
     city: string;
-
-    @Transform(({ value }) => value.trim())
-    @Transform(({ value }) => value.toLowerCase())
-    @IsString()
-    @MinLength(1)
-    state: string;
 
     @Transform(({ value }) => value.trim())
     @IsString()
