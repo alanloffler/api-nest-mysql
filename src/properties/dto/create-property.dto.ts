c8c7a8494133b5f7172a6b1d80177c01610b7f26
alanloffler <alanmatiasloffler@gmail.com>
2024-05-25 22:59:12 -0300

    Changed IsNumber to IsDecimal

--- src/properties/dto/create-property.dto.ts
+++ src/properties/dto/create-property.dto.ts
@@ -1,4 +1,4 @@
-import { IsNumber, IsPositive, IsString, IsIn, MinLength, Max, IsInt, IsOptional } from 'class-validator';
+import { IsNumber, IsPositive, IsString, IsIn, MinLength, Max, IsInt, IsOptional, IsDecimal } from 'class-validator';
 
 export class CreatePropertyDto {
     @IsString()
@@ -49,11 +49,11 @@ export class CreatePropertyDto {
     @MinLength(1)
     zip: string
 
-    @IsNumber()
+    @IsDecimal()
     @IsOptional()
     lat?: number;
 
-    @IsNumber()
+    @IsDecimal()
     @IsOptional()
     lng?: number;
 
