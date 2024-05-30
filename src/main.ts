87f6addd6d3ca4faadb2b8700ca350bf242208f8
alanloffler <alanmatiasloffler@gmail.com>
2024-05-30 17:01:07 -0300

    cors

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
     const app = await NestFactory.create<NestExpressApplication>(AppModule);
-    app.enableCors();
+    app.enableCors({ origin: '*' });
     app.setGlobalPrefix('api');
     app.useGlobalPipes(
         new ValidationPipe({
