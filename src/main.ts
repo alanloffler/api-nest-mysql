7b343f39e92f0323a9a2350588aab399cd761ed4
alanloffler <alanmatiasloffler@gmail.com>
2024-05-31 18:22:30 -0300

    Serve static files (for images)

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,15 @@
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
+import * as express from 'express';
 import { AppModule } from './app.module';
+import { join } from 'path';
 
 async function bootstrap() {
     const app = await NestFactory.create<NestExpressApplication>(AppModule);
     app.enableCors({
         origin: '*',
+        allowedHeaders: '*',
         credentials: true,
         methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     });
@@ -18,6 +21,9 @@ async function bootstrap() {
             transform: true,
         }),
     );
+
+    app.use('/photos', express.static(join(__dirname, '..', 'photos')));
+
     await app.listen(parseInt(process.env.PORT) || 4000);
 }
 bootstrap();
